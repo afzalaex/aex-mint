9ad49a33cd970d6a7e36d410774ba5fab8424778
Afzal <afzalaex@gmail.com>
2026-04-18 02:17:36 +0530

    feat(icons): add SVG maximize icon for canvas enlarge button; fix close button to skip fill hover effect; remove hardcoded icon color so icons inherit currentColor and turn black correctly on button hover

--- components/Icon.vue
+++ components/Icon.vue
@@ -4,6 +4,12 @@
     <line x1="2" y1="12" x2="22" y2="12"></line>
     <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
   </svg>
+  <svg v-else-if="type === 'maximize'" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
+    <polyline points="15 3 21 3 21 9"></polyline>
+    <polyline points="9 21 3 21 3 15"></polyline>
+    <line x1="21" y1="3" x2="14" y2="10"></line>
+    <line x1="3" y1="21" x2="10" y2="14"></line>
+  </svg>
   <i v-else-if="ICONS[type]" class="icon">
     {{ symbol }}
   </i>
@@ -49,6 +55,5 @@ const symbol = computed(() => ICONS[props.type])
 .icon {
   display: inline-block;
   font-style: normal;
-  color: #ffffff;
 }
 </style>
